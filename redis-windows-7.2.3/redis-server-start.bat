340b8684b42679faf64373c9239e6354c5a92d23
OuYangXiaoPeng <2096008975@qq.com>
2025-10-11 11:45:20 +0800

    上传

---
+++ redis-windows-7.2.3/redis-server-start.bat
@@ -0,0 +1,6 @@
+start redis-server redis-7000.conf
+start redis-server redis-7001.conf
+start redis-server redis-7002.conf
+start redis-server redis-7003.conf
+start redis-server redis-7004.conf
+start redis-server redis-7005.conf
